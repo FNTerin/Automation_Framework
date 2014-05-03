a3b038bc7733121ae105efde684f48dace46d90b
FatimaNTerin <fatima.terin@gmail.com>
2014-05-03 15:30:34 -0400

    editing pom

--- generic/src/main/java/base/Base.java
+++ generic/src/main/java/base/Base.java
@@ -1,15 +1,23 @@
 package base;
 
-import org.junit.After;
-import org.junit.Before;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterClass;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -24,6 +32,50 @@ import java.util.concurrent.TimeUnit;
 public class Base {
 
     public  WebDriver driver = null;
+    @Parameters({"useSauceLabs","userName", "key", "os", "browser", "browserVersion","url"})
+    @BeforeClass
+    public void setUp(boolean useSauceLabs,String userName,String key,String os, String browser,String browserVersion,
+                      String url) throws IOException {
+        if(useSauceLabs==true){
+            setUpSauce(userName,key,os,browser,browserVersion,url);
+        }else{ getDriver(browser,url);
+
+        }
+    }
+    //driver to run on SauceLabs
+    public void setUpSauce(String userName,String key,String os, String browser,String browserVersion,
+                           String url) throws IOException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName(browser);
+        capabilities.setCapability("version", browserVersion);
+        capabilities.setCapability("platform", os);
+        this.driver = new RemoteWebDriver(
+                new URL("http://" + userName + ":" + key + "@ondemand.saucelabs.com:80/wd/hub"),
+                capabilities);
+        driver.navigate().to(url);
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    //driver to run on local
+    public WebDriver getDriver(String browser,String url) {
+        if(browser.equalsIgnoreCase("firefox")){
+            driver = new FirefoxDriver();
+        } else if(browser.equalsIgnoreCase("safari")){
+            driver = new SafariDriver();
+        } else if(browser.equalsIgnoreCase("chrome")){
+            System.setProperty("webdriver.chrome.driver", System.setProperty("path","path"));
+            driver = new ChromeDriver();
+        } else if(browser.equalsIgnoreCase("internetexplorer")){
+            driver = new InternetExplorerDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        driver.navigate().to(url);
+        driver.manage().window().maximize();
+
+        return driver;
+    }
 
 
     @BeforeClass
